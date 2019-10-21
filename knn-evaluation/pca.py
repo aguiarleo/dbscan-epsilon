81cfd554ac3cfb9d56c6a14edaf381c68d5707e5
Leonardo <leonardodeconti@gmail.com>
2019-10-20 23:58:07 -0300

    Possibilidade de fazer a decomposicao com n componentes.

--- knn-evaluation/pca.py
+++ knn-evaluation/pca.py
@@ -1,7 +1,10 @@
 def pca_2_components(data):
+	return pca_decompose(data,2)
+	
+def pca_decompose(data,n_components):
 	from sklearn.decomposition import PCA, IncrementalPCA
-	print("[i] Decomposing to 2 components by PCA ...", end=" ")
-	ipca = IncrementalPCA(n_components=2, batch_size=10)
+	print("[i] Decomposing to {} components by PCA ...".format(n_components), end=" ")
+	ipca = IncrementalPCA(n_components=n_components, batch_size=10)
 	data_ipca = ipca.fit_transform(data)
 	print("Done!")
 	return data_ipca
